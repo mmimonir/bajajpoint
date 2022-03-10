2633c793475353b513a010302fcf387531cdb616
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2022-03-10 17:25:27 +0600

    bug fixes

--- resources/views/dms/quotations/quotation_list.blade.php
+++ resources/views/dms/quotations/quotation_list.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 @section('title', 'Quotation List')
+@section('datatable_css')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css" />
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css" />
+@endsection
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
@@ -22,8 +26,8 @@
                 <!-- /.container-fluid -->
             </div>
             <div class="card mt-2" style="box-shadow: 0 0 25px 0 lightgrey">
-                <div class="card-body">
-                    <table id="example" class="table table-hover table-sm table-bordered">
+                <div class="card-body container h-100 d-flex justify-content-center">
+                    <table id="example" class="table table-hover table-responsive table-striped table-sm text-sm table-light table-bordered" style="width:100%;">
                         <thead class="text-center">
                             <tr class="text-sm">
                                 <th>Sl</th>
@@ -222,8 +226,16 @@
 </div>
 <!-- Modal Update End-->
 @endsection @section('datatable')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
 @endsection @section('script')
 <script>
     $('#add').on('click', function(e) {
@@ -250,10 +262,13 @@
         }
     }
     $("#example").DataTable({
-        bPaginate: false,
         pageLength: 10,
         responsive: true,
         lengthChange: true,
+        dom: '<"html5buttons"B>lTfgitp',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
     });
     $(document).on('click', '.editIcon', function(e) {
         $('#quotation_update_form').trigger('reset');
