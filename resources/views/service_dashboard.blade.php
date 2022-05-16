22971a65ac56735f1d1626d64896e7bde104deed
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2022-05-16 22:13:00 +0600

    some changes

--- resources/views/service_dashboard.blade.php
+++ resources/views/service_dashboard.blade.php
@@ -4,9 +4,9 @@
 <div class="container-fluid" style="margin-top:15px; padding:0;">
     <div class="row">
         <div class="col-md-12">
-            <div class="card card-success collapsed-card">
+            <div class="card card-success">
                 <div class="card-header bg-dark">
-                    <h3 class="card-title">Search Together</h3>
+                    <h3 class="card-title">Bajaj Point - Service Dashboard</h3>
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="maximize">
                             <i class="fas fa-expand"></i>
@@ -19,41 +19,63 @@
                         </button>
                     </div>
                 </div>
-                <div class="card-body" style="display: block; padding:0px;">
-                    <div class="card mt-1" style="box-shadow: 0 0 5px 0 lightgrey">
-                        <div class="card-header bg-dark d-flex justify-content-center">
-                            <h3 class="card-title">
-                                Sales Update, Hform, Stamp, Gate Pass, Single
-                                Print
-                            </h3>
-                        </div>
-                        <div class="card-body d-flex justify-content-center">
-                            <form class="form-inline" action="#" method="post" id="search_form">
-                                @csrf
-                                <div class="form-group mb-2">
-                                    <label for="five_chassis">Chassis No</label>
-                                </div>
-                                <div class="form-group mx-sm-3 mb-2">
-                                    <input type="text" name="five_chassis" class="form-control" id="five_chassis" placeholder="Last Five Digit Chassis">
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="five_engine">Engine No</label>
-                                </div>
-                                <div class="form-group mx-sm-3 mb-2">
-                                    <input type="text" name="five_engine" class="form-control" id="five_engine" placeholder="Last Five Digit Engine">
-                                </div>
-                                <button type="submit" class="btn btn-dark mb-2">
-                                    Search
-                                </button>
-                            </form>
-                        </div>
+                <div class="card-body">
+                    <div class="col-md-12 col-sm-6 d-flex justify-content-center">
+                        <a class="btn btn-inline bg-dark" style="width:150px; margin:0 5px;">
+                            Create Invoice
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:150px; margin:0 5px;">
+                            Make Requisitions
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:150px; margin:0 5px;">
+                            Add Mechanics
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:150px; margin:0 5px;">
+                            Create Job Card
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:150px; margin:0 5px;">
+                            Purchage Entry
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:150px; margin:0 5px;">
+                            Add Supplier
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:150px; margin:0 5px;">
+                            Current Stock
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:150px; margin:0 5px;">
+                            Current Stock
+                        </a>
                     </div>
-                    <div class="col-md-12" style="margin-top:-15px; padding:0px;">
-                        <div class="card mt-2" style="box-shadow: 0 0 25px 0 lightgrey; margin-bottom:0px;" id="show_search_result">
-
-                        </div>
-                    </div>
+                    <!-- <div style="height:1px;"></div>
+                    <div class="col-md-12 d-flex justify-content-center">
+                        <a class="btn btn-inline bg-dark" style="width:9rem; margin:0 8px;">
+                            Create Invoice
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:9rem; margin:0 8px;">
+                            Make Requisitions
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:9rem; margin:0 8px;">
+                            Add Mechanics
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:9rem; margin:0 8px;">
+                            Create Job Card
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:9rem; margin:0 8px;">
+                            Purchage Entry
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:9rem; margin:0 8px;">
+                            Add Supplier
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:9rem; margin:0 8px;">
+                            Current Stock
+                        </a>
+                        <a class="btn btn-inline bg-dark" style="width:9rem; margin:0 8px;">
+                            Current Stock
+                        </a>
+                    </div> -->
                 </div>
+
+                <!-- Content Here -->
                 <!-- <div class="overlay" id="search_overlay">
                     <i class="fas fa-2x fa-sync-alt fa-spin"></i>
                 </div> -->
