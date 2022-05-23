1ca90c98d07c4e1d230e8a2945a9c575c2bbda2f
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2022-05-23 22:01:05 +0600

    job card editing

--- resources/views/service/job_card.blade.php
+++ resources/views/service/job_card.blade.php
@@ -3,7 +3,14 @@
 
 @push('page_css')
 <style>
+    * {
+        box-sizing: border-box;
+    }
 
+    .custom-img {
+        width: 100px;
+        margin-right: -50px;
+    }
 </style>
 @endpush
 @section('content')
@@ -20,15 +27,79 @@
             <div class="card-body bangla_font bangla_font_light">
                 <table class="table table-bordered job_card_table">
                     <tr>
-                        <td>Logo</td>
+                        <td class="align-middle">
+                            <div class="custom-img">
+                                <img src="{{URL('/images/uml_logo.png')}}" style="width:100%;">
+                            </div>
+                        </td>
                         <td class="text-center">
-
                             <p class="m-0 font-weight-bold h3"><span class="fs-4">ডিলারের নাম: </span>বাজাজ পয়েন্ট (বাজাজ সার্ভিস সেন্টার)</p>
                             <p class="m-0 font-weight-bold">400/বি, মালিবাগ চৌধুরী পাড়া, ঢাকা-1219</p>
                             <p class="m-0 font-weight-bold">মোবাইল: 01680 365 200, 01813 551 621</p>
                         </td>
-                        <td>জব কার্ড নং:</td>
-                        <td>Logo</td>
+                        <td class="align-middle">
+                            <p class="m-0 font-weight-bold">জব কার্ড নং:</p>
+                            <p class="m-0 font-weight-bold">জব কার্ড তারিখ:</p>
+                        </td>
+                        <td class="align-middle">
+                            <div class="custom-img">
+                                <img src="{{URL('/images/bajaj_logo.png')}}" style="width:100%;">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="4" style="padding:0;">
+                            <div class="row">
+                                <div class="col-md-4 pr-0">
+                                    <div>
+                                        <p class="p-1 border m-0 font-weight-bold">গাড়ির বিবরণ</p>
+                                        <p class="p-1 border m-0">গাড়ির রেজিঃ নম্বর:<input class="ml-1" style="width:300px; height:21px; border:0px;" type="text"></p>
+                                        <p class="p-1 border m-0">গাড়ির মডেল:<input class="ml-1" style="width:300px; height:21px; border:0px;" type="text"></p>
+                                        <p class="p-1 border m-0"><span style="margin-right:150px;">বিক্রয় তারিখ:</span><span>মাইলেজ:</span></p>
+                                        <p class="p-1 border m-0"><span style="margin-right:150px;">ইঞ্জিন নং:</span><span>চেসিস নং:</span></p>
+                                        <p class="p-1 border m-0">
+                                            <span style="margin-right:50px;">সার্ভিসের ধরণ:</span>
+                                            <span style="margin-right:25px;">পেইড সার্ভিস </span><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                            <span style="margin-right:25px;">ফ্রি সার্ভিস</span><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        </p>
+                                        <p class="p-1 border m-0"><span style="margin-right:50px;">মাইনর মেরামত</span><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></p>
+                                        <p class="p-1 border m-0"><span style="margin-right:50px;">মেজর মেরামত</span><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></p>
+                                        <p class="p-1 border m-0"><span style="margin-right:50px;">ইঞ্জিন ওভারহোলিং</span><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></p>
+                                        <p class="p-1 border m-0"><span style="margin-right:50px;">দূর্ঘটনাজনিত মেরামত</span><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 pl-0">
+                                    <p class="p-1 border m-0 font-weight-bold">গ্রাহকের নাম:</p>
+                                    <p class="p-1 border m-0">ঠিকানা:</p>
+                                    <p class="p-1 border m-0">-</p>
+                                    <p class="p-1 border m-0">টেলিফোন নম্বর:</p>
+                                    <p class="p-1 border m-0 font-weight-bold">গাড়ি পর্যবেক্ষণের বিবরণ:</p>
+                                    <p class="p-1 border m-0">
+                                        <span>ফুয়েলের পরিমাণ:
+                                            <input class="ml-1" style="width:45px; height:20px; margin-right:150px;" type="text">
+                                        </span>
+                                        <span style="margin-right:20px;">ফুয়েল ট্যাংকে দাগ আছে কিনা?</span>
+                                        <span style="margin-right:30px;">হ্যাঁ</span>
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <span style="margin-right:30px;">না</span>
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                    </p>
+                                    <p class="p-1 border m-0">
+                                        <span style="margin-right:20px;">ইন্ডিকেটরের লেন্স ভাঙ্গা কিনা?</span>
+                                        <span style="margin-right:30px;">হ্যাঁ</span>
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <span style="margin-right:30px;">না</span>
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+
+                                        <span style="margin-right:20px; margin-left:25px;">হেডলাইটে দাগ আছে কিনা?</span>
+                                        <span style="margin-right:30px;">হ্যাঁ</span>
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <span style="margin-right:30px;">না</span>
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                    </p>
+                                </div>
+                            </div>
+                        </td>
                     </tr>
                 </table>
             </div>
