b7d0dd1556754b87958771e481183e1991be8e99
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2022-09-05 01:26:43 +0600

    employee ui download

--- resources/views/dms/attendance/attendance.blade.php
+++ resources/views/dms/attendance/attendance.blade.php
@@ -55,40 +55,40 @@
                                 <option selected>
                                     Month
                                 </option>
-                                <option value="January">
+                                <option value="1">
                                     January
                                 </option>
-                                <option value="February">
+                                <option value="2">
                                     February
                                 </option>
-                                <option value="March">
+                                <option value="3">
                                     March
                                 </option>
-                                <option value="April">
+                                <option value="4">
                                     April
                                 </option>
-                                <option value="May">
+                                <option value="5">
                                     May
                                 </option>
-                                <option value="June">
+                                <option value="6">
                                     June
                                 </option>
-                                <option value="July">
+                                <option value="7">
                                     July
                                 </option>
-                                <option value="August">
+                                <option value="8">
                                     August
                                 </option>
-                                <option value="September">
+                                <option value="9">
                                     September
                                 </option>
-                                <option value="October">
+                                <option value="10">
                                     October
                                 </option>
-                                <option value="November">
+                                <option value="11">
                                     November
                                 </option>
-                                <option value="December">
+                                <option value="12">
                                     December
                                 </option>
                             </select>
@@ -115,6 +115,8 @@
                                 <tr>
                                     <td>Albina Simonis</td>
                                     @for ($i = 1; $i <= 31; $i++) <td style="padding:0; vertical-align: middle; text-align:center;">
+                                        <input type="hidden" name="employee_id" />
+                                        <input type="hidden" name="day" Value="{{$i}}" />
                                         <select class="select attendance" name="attendance[]">
                                             <option></option>
                                             <option value="A">A</option>
@@ -124,6 +126,195 @@
                                         </select>
                                         </td>
                                         @endfor
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xl-12 col-sm-12 stretch-card grid-margin">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-sm-flex justify-content-between align-items-center">
+                        <div class="px-5 py-4 py-lg-0 border-right-lg w-100">
+                            <h4 class="mb-0">Payroll</h4>
+                            <p class="mb-0">29 calender days</p>
+                        </div>
+                        <div class="px-5 py-4 py-lg-0 border-right-lg w-100">
+                            <h5 class="mb-0 font-weight-normal text-muted">Total employees</h5>
+                            <div class="d-flex align-items-center">
+                                <h1 class=" mb-0 mr-2">130</h1>
+                                <h4 class="text-success font-weight-normal mb-0">+2</h4>
+                            </div>
+                        </div>
+                        <div class="px-5 py-4 py-lg-0 border-right-lg w-100">
+                            <h5 class="mb-0 font-weight-normal text-muted">Working days</h5>
+                            <div class="d-flex align-items-center">
+                                <h1 class=" mb-0 mr-2">24</h1>
+                            </div>
+                        </div>
+                        <div class="px-5 py-4 py-lg-0 w-100">
+                            <h5 class="mb-0 font-weight-normal text-muted">Payroll Processed</h5>
+                            <div class="d-flex align-items-center">
+                                <h1 class=" mb-0 mr-2">121</h1>
+                                <h4 class="font-weight-normal mb-0">/130</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xl-12 stretch-card grid-margin">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between flex-wrap mb-3">
+                        <div>
+                            <div class="card-title mb-0">Logs &amp; Requests</div>
+                        </div>
+                        <div>
+                            <button class="btn btn-light btn-sm text-dark">Attendance Log</button>
+                            <button class="btn btn-light btn-sm text-dark">Attendance Requests</button>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th> Date </th>
+                                    <th> Leave </th>
+                                    <th> Effective Hours </th>
+                                    <th> Gross Hours </th>
+                                    <th>Arrival</th>
+                                    <th>Log</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>12 Sep 2020 </td>
+                                    <td>
+                                        SunW-OFF
+                                    </td>
+                                    <td>
+                                        6:18 + hrs
+                                    </td>
+                                    <td> 6:18 + hrs</td>
+                                    <td> 0:42:37 late</td>
+                                    <td><i class="mdi mdi-check-circle mdi-18px text-success mr-2"></i>Present</td>
+                                </tr>
+                                <tr>
+                                    <td>14 Mar 2020 </td>
+                                    <td>Leave</td>
+                                    <td>8:18 hrs</td>
+                                    <td>8:18 hrs</td>
+                                    <td>0:06:25 late</td>
+                                    <td><i class="mdi mdi-check-circle mdi-18px text-success mr-2"></i>Present</td>
+                                </tr>
+                                <tr>
+                                    <td>26 Feb 2020</td>
+                                    <td>Unpaid Leave</td>
+                                    <td>7:09 hrs</td>
+                                    <td>7:09 hrs</td>
+                                    <td>0:38:13 late</td>
+                                    <td><i class="mdi mdi-check-circle mdi-18px text-success mr-2"></i>Present</td>
+                                </tr>
+                                <tr>
+                                    <td>22 Apr 2020</td>
+                                    <td> Unpaid Leave </td>
+                                    <td>6:42 hrs</td>
+                                    <td>6:42 hrs</td>
+                                    <td>0:12:27 late</td>
+                                    <td><i class="mdi mdi-information mdi-18px text-danger mr-2"></i>Out missing</td>
+                                </tr>
+                                <tr>
+                                    <td>26 Oct 2020</td>
+                                    <td>12 Sep 2020</td>
+                                    <td>8:09 hrs</td>
+                                    <td>8:09 hrs</td>
+                                    <td>0:49:23 late</td>
+                                    <td><i class="mdi mdi-check-circle mdi-18px text-success mr-2"></i>Present</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 stretch-card grid-margin">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-xl-flex justify-content-between mb-3">
+                        <div>
+                            <div class="card-title mb-0">Interviews <span class="text-small text-muted font-weight-normal ml-2">view all</span></div>
+                        </div>
+                        <div>
+                            <button class="btn btn-link btn-sm text-dark">Today</button>
+                            <button class="btn btn-link btn-sm text-muted">Comming</button>
+                            <button class="btn btn-link btn-sm text-muted">Completed</button>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th> Candidate </th>
+                                    <th> Interview Status </th>
+                                    <th> Interviewer </th>
+                                    <th> Schedule </th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Francisco Maia <span class="text-small">(Designer)</span> </td>
+                                    <td>
+                                        <div class="badge badge-primary badge-sm">active</div>
+                                    </td>
+                                    <td>
+                                        Phillips <span class="text-small"> (Creative Head)</span>
+                                    </td>
+                                    <td> Technical Aptitude</td>
+                                    <td> <button class="btn btn-info btn-sm">send reminder</button></td>
+                                </tr>
+                                <tr>
+                                    <td>Fua Lamba <span class="text-small">(Designer)</span> </td>
+                                    <td>
+                                        <div class="badge badge-success badge-sm">final</div>
+                                    </td>
+                                    <td>
+                                        Jelanee <span class="text-small"> (Lead Dev)</span>
+                                    </td>
+                                    <td> Face- to- face interview</td>
+                                    <td> <button class="btn btn-info btn-sm">send reminder</button></td>
+                                </tr>
+                                <tr>
+                                    <td>Lacara Jones <span class="text-small">(Designer)</span> </td>
+                                    <td>
+                                        <div class="badge badge-primary badge-sm">active</div>
+                                    </td>
+                                    <td>
+                                        Phillips <span class="text-small"> (Creative Head)</span>
+                                    </td>
+                                    <td> Telephonic interview</td>
+                                    <td> <button class="btn btn-info btn-sm">send reminder</button></td>
+                                </tr>
+                                <tr>
+                                    <td>Langke Zambo <span class="text-small">(Designer)</span> </td>
+                                    <td>
+                                        <div class="badge badge-danger badge-sm">beginner</div>
+                                    </td>
+                                    <td>
+                                        Phillips <span class="text-small"> (Creative Head)</span>
+                                    </td>
+                                    <td> Discussion with CEO</td>
+                                    <td> <button class="btn btn-info btn-sm">send reminder</button></td>
                                 </tr>
                             </tbody>
                         </table>
@@ -151,7 +342,15 @@
     $(document).ready(function() {
         $(document).on('change', '.attendance', function() {
             _this = $(this);
+            let parent_td = $(this).parent();
+            let attendance_day = parent_td.find('input[name="day"]').val();
+            let employee_id = parent_td.find('input[name="employee_id"]').val();
             let attendance = $(this).val();
+            let attendance_month = $('#attendance_month').val();
+            let attendance_year = $('#attendance_year').val();
+
+            // console.log(attendance_day, employee_id, attendance, attendance_month, attendance_year);
+
             if (attendance == 'A') {
                 _this.css('background-color', 'red').css('color', 'white').css('border', 'none');
             } else if (attendance == 'P') {
@@ -160,6 +359,8 @@
                 _this.css('background-color', 'black').css('color', 'white').css('border', 'none');
             } else if (attendance == 'H') {
                 _this.css('background-color', 'purple').css('color', 'white').css('border', 'none');
+            } else if (attendance == '') {
+                _this.css('background-color', '#E6E6E6').css('color', 'black').css('border', '1px solid green');
             }
 
         });
@@ -178,6 +379,8 @@
             }
         }
         create_attendance_year();
+        $('#attendance_year').val(new Date().getFullYear());
+        $('#attendance_month').val(new Date().getMonth() + 1);
     });
 </script>
 @endsection
