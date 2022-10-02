24d3557ad4e51b507a1d9322cc2c5fbaa7bd33ba
mmimonir <mmimonir.ctg@gmail.com>
2022-10-02 20:19:47 +0600

    dashboard layout change

--- resources/views/dms/vat_dashboard.blade.php
+++ resources/views/dms/vat_dashboard.blade.php
@@ -12,6 +12,223 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
+                            <h1 class="m-0">View List</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card mt-2" style="box-shadow: 0 0 25px 0 lightgrey">
+                        <div class="card-header bg-dark">
+                            <h3 class="text-center rounded">Gen. Sale Mushak No</h3>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-12">
+                                <div class="card-body">
+                                    <form action="{{ route('vat.assign_sale_mushak_no') }}" method="post" target="_blank">
+                                        @csrf
+                                        <div class="form-group row">
+                                            <label for="inputEmail3" class="col-sm-4 col-form-label">Dealer</label>
+                                            <div class="col-sm-8">
+                                                <select name="vat_code" class="browser-default custom-select" required>
+                                                    <option selected>
+                                                        Select Dealer Name
+                                                    </option>
+                                                    @foreach ($dealer as $data)
+                                                    <option value="{{$data->supplier_code}}">
+                                                        {{$data->dealer_name}}
+                                                    </option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="start_date" class="col-sm-4 col-form-label">Start Date</label>
+                                            <div class="col-sm-8">
+                                                <input required type="date" class="form-control" id="start_date" name="start_date" placeholder="Start Date" />
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="end_date" class="col-sm-4 col-form-label">End Date</label>
+                                            <div class="col-sm-8">
+                                                <input required type="date" class="form-control" id="end_date" name="end_date" placeholder="End Date" />
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-sm-12 text-center">
+                                                <button type="submit" class="btn btn-dark btn-block">
+                                                    View
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card mt-2" style="box-shadow: 0 0 25px 0 lightgrey">
+                        <div class="card-header bg-dark">
+                            <h3 class="text-center rounded">VAT Sale By Model</h3>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-12">
+                                <div class="card-body">
+                                    <form action="{{ route('vat.vat_sale_by_model') }}" method="post" target="_blank">
+                                        @csrf
+                                        <div class="form-group row">
+                                            <label for="inputEmail3" class="col-sm-4 col-form-label">Dealer</label>
+                                            <div class="col-sm-8">
+                                                <select name="vat_code" class="browser-default custom-select" required>
+                                                    <option selected>
+                                                        Select Dealer Name
+                                                    </option>
+                                                    @foreach ($dealer as $data)
+                                                    <option value="{{$data->supplier_code}}">
+                                                        {{$data->dealer_name}}
+                                                    </option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="start_date" class="col-sm-4 col-form-label">Start Date</label>
+                                            <div class="col-sm-8">
+                                                <input required type="date" class="form-control" id="start_date" name="start_date" placeholder="Start Date" />
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="end_date" class="col-sm-4 col-form-label">End Date</label>
+                                            <div class="col-sm-8">
+                                                <input required type="date" class="form-control" id="end_date" name="end_date" placeholder="End Date" />
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-sm-12 text-center">
+                                                <button type="submit" class="btn btn-dark btn-block">
+                                                    Print
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card mt-2" style="box-shadow: 0 0 25px 0 lightgrey">
+                        <div class="card-header bg-dark">
+                            <h3 class="text-center rounded">VAT Purchage</h3>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-12">
+                                <div class="card-body">
+                                    <form action="{{ route('vat.vat_purchage_homepage') }}" method="post" target="_blank">
+                                        @csrf
+                                        <div class="form-group row">
+                                            <label for="inputEmail3" class="col-sm-4 col-form-label">Dealer</label>
+                                            <div class="col-sm-8">
+                                                <select name="vat_code" class="browser-default custom-select" required>
+                                                    <option selected>
+                                                        Select Dealer Name
+                                                    </option>
+                                                    @foreach ($dealer as $data)
+                                                    <option value="{{$data->supplier_code}}">
+                                                        {{$data->dealer_name}}
+                                                    </option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="start_date" class="col-sm-4 col-form-label">Start Date</label>
+                                            <div class="col-sm-8">
+                                                <input required type="date" class="form-control" id="start_date" name="start_date" placeholder="Start Date" />
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="end_date" class="col-sm-4 col-form-label">End Date</label>
+                                            <div class="col-sm-8">
+                                                <input required type="date" class="form-control" id="end_date" name="end_date" placeholder="End Date" />
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-sm-12 text-center">
+                                                <button type="submit" class="btn btn-dark btn-block">
+                                                    Print
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card mt-2" style="box-shadow: 0 0 25px 0 lightgrey">
+                        <div class="card-header bg-dark">
+                            <h3 class="text-center rounded">VAT Sale</h3>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-12">
+                                <div class="card-body">
+                                    <form action="{{ route('vat.vat_sale') }}" method="post" target="_blank">
+                                        @csrf
+                                        <div class="form-group row">
+                                            <label for="inputEmail3" class="col-sm-4 col-form-label">Dealer</label>
+                                            <div class="col-sm-8">
+                                                <select name="vat_code" class="browser-default custom-select" required>
+                                                    <option selected>
+                                                        Select Dealer Name
+                                                    </option>
+                                                    @foreach ($dealer as $data)
+                                                    <option value="{{$data->supplier_code}}">
+                                                        {{$data->dealer_name}}
+                                                    </option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="start_date" class="col-sm-4 col-form-label">Start Date</label>
+                                            <div class="col-sm-8">
+                                                <input required type="date" class="form-control" id="start_date" name="start_date" placeholder="Start Date" />
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="end_date" class="col-sm-4 col-form-label">End Date</label>
+                                            <div class="col-sm-8">
+                                                <input required type="date" class="form-control" id="end_date" name="end_date" placeholder="End Date" />
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-sm-12 text-center">
+                                                <button type="submit" class="btn btn-dark btn-block">
+                                                    Print
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
                             <h1 class="m-0">Dashboard</h1>
                         </div>
                     </div>
@@ -157,72 +374,7 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">View List</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="card mt-2" style="box-shadow: 0 0 25px 0 lightgrey">
-                        <div class="card-header bg-dark">
-                            <h3 class="text-center rounded">Gen. Sale Mushak No</h3>
-                        </div>
-                        <div class="row justify-content-center">
-                            <div class="col-md-12">
-                                <div class="card-body">
-                                    <form action="{{ route('vat.assign_sale_mushak_no') }}" method="post" target="_blank">
-                                        @csrf
-                                        <div class="form-group row">
-                                            <label for="inputEmail3" class="col-sm-4 col-form-label">Dealer</label>
-                                            <div class="col-sm-8">
-                                                <select name="vat_code" class="browser-default custom-select" required>
-                                                    <option selected>
-                                                        Select Dealer Name
-                                                    </option>
-                                                    @foreach ($dealer as $data)
-                                                    <option value="{{$data->supplier_code}}">
-                                                        {{$data->dealer_name}}
-                                                    </option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label for="start_date" class="col-sm-4 col-form-label">Start Date</label>
-                                            <div class="col-sm-8">
-                                                <input required type="date" class="form-control" id="start_date" name="start_date" placeholder="Start Date" />
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label for="end_date" class="col-sm-4 col-form-label">End Date</label>
-                                            <div class="col-sm-8">
-                                                <input required type="date" class="form-control" id="end_date" name="end_date" placeholder="End Date" />
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <div class="col-sm-12 text-center">
-                                                <button type="submit" class="btn btn-dark btn-block">
-                                                    View
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="row justify-content-center">
                 <div class="col-md-3">
                     <div class="card mt-2" style="box-shadow: 0 0 25px 0 lightgrey">
                         <div class="card-header bg-dark">
@@ -298,6 +450,7 @@
         </div>
     </div>
 </div>
+
 
 @endsection
 @section('script')
