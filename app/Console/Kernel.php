147e08eb36addba83f3d0a03eb5f11c8bb52a15e
Md Monirul Islam <mmimonir.ctg@gmail.com>
2023-01-03 20:55:15 +0600

    backup run edited

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('backup:run --only-db')->everyMinute();
+        // $schedule->command('backup:run --only-db')->everyMinute();
+        $schedule->command('backup:run --only-db');
     }
 
     /**
@@ -26,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
