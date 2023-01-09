0c9ad32a2f848f80ab2fe796a4e7bd16087b6806
Md Monirul Islam <mmimonir.ctg@gmail.com>
2023-01-09 21:30:41 +0600

    mail send testing ok

--- resources/views/emails/user_login_alert.blade.php
+++ resources/views/emails/user_login_alert.blade.php
@@ -1,9 +1,9 @@
 @component('mail::message')
-Hello {{$user->name}},
+Hello Dear Admin!!
 
+Your Employee {{$user->name}} just logged in to your account at {{ config('app.name') }}.
 
-You can view your order details using the link below.
-
+It's a good practice to keep your account secure. If you didn't authorize this login, please change your password immediately.
 
 Regards,<br>
 {{ config('app.name') }}
