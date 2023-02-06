0dbc6e22ec9432abf4e39140e3ccc1f2ecb69fe0
Md Monirul Islam <mmimonir.ctg@gmail.com>
2023-02-06 20:32:08 +0600

    tr challan update

--- resources/views/dms/vat/tr_challan.blade.php
+++ resources/views/dms/vat/tr_challan.blade.php
@@ -26,7 +26,7 @@
         th {
             border: 1px solid #595959;
             border-collapse: collapse;
-            padding: 10px;
+            padding: 6px;
             text-align: center;
             vertical-align: baseline;
         }
@@ -34,15 +34,17 @@
         .tr_page {
             /* height: 1410px; */
             /* width: 1020px; */
-            height: 1020px;
-            width: 1410px;
+            height: 965px;
+            /* 7.3 inch (132px); */
+            width: 1425px;
+            /* 10.8 inch (132px); */
             border: 1px solid black;
             margin: auto;
             position: relative;
-            padding: 10px;
+            /* padding: 10px; */
             /* box-sizing: border-box; */
             box-shadow: 0 0 25px 0 lightgrey;
-            margin: 15px auto;
+            /* margin: 15px auto; */
         }
 
         .left_chassis,
@@ -50,6 +52,9 @@
             display: block;
             width: 244.8px;
             text-align: justify;
+            height: 480px;
+            font-size: 14px;
+            text-align-last: center;
         }
 
         .sl {
@@ -74,15 +79,15 @@
         }
 
         .code_box {
-            width: 68px;
-            height: 41px;
+            width: 34px;
+            height: 34px;
             border: 1px solid #595959;
             display: inline-block;
-            padding: 9px;
+            padding: 6px;
         }
 
         .box_margin_left {
-            margin-left: 50px;
+            margin-left: 25px;
         }
 
         .code_no {
@@ -99,6 +104,25 @@
             padding-top: 30px;
             padding-bottom: 5px;
         }
+
+        .f-bold {
+            font-weight: bold;
+        }
+
+        .border_top_style {
+            border-top-style: hidden;
+        }
+
+        .trade_vat {
+            font-size: 12px;
+            padding: 3px;
+            font-weight: bold;
+        }
+
+        .border_right_zero {
+            margin-right: -5px;
+            border-right: 0;
+        }
     </style>
 </head>
 
@@ -107,20 +131,21 @@
         <div class="text_center header_content">
             <h2>চালান ফরম</h2>
             <p>টি, আর ফরম নং 6 (এস.আর 37 দ্রষ্টব্য)</p>
+            <p style="padding:5px;">চালান নং.............................. তারিখ..............................</p>
             <p>বাংলাদেশ ব্যাংক/সোনালী ব্যাংক লিমিটেডের <span>DHAKA</span> জেলার <span>KHILGAON</span> শাখায় টাকা জমা দেওয়ার চালান</p>
             <p class="code_no">কোড নং
                 <span class="code_box">1</span>
-                <span class="code_box box_margin_left">1</span>
-                <span class="code_box">1</span>
+                <span class="code_box box_margin_left border_right_zero">1</span>
+                <span class="code_box border_right_zero">1</span>
+                <span class="code_box border_right_zero">3</span>
                 <span class="code_box">3</span>
-                <span class="code_box">3</span>
-                <span class="code_box box_margin_left">0</span>
+                <span class="code_box box_margin_left border_right_zero">0</span>
+                <span class="code_box border_right_zero">0</span>
+                <span class="code_box border_right_zero">1</span>
                 <span class="code_box">0</span>
-                <span class="code_box">1</span>
-                <span class="code_box">0</span>
-                <span class="code_box box_margin_left">0</span>
-                <span class="code_box">3</span>
-                <span class="code_box">1</span>
+                <span class="code_box box_margin_left border_right_zero">0</span>
+                <span class="code_box border_right_zero">3</span>
+                <span class="code_box border_right_zero">1</span>
                 <span class="code_box">1</span>
             </p>
         </div>
@@ -136,22 +161,32 @@
                     <td>যে ব্যক্তির/প্রতিষ্ঠানের পক্ষ হইতে টাকা প্রদত্ত হইল তাহার নাম, পদবী ও ঠিকানা</td>
                     <td colspan="2" class="vertical_align_middle">কি বাবদ জমা দেওয়া হইল তাহার বিবরণ</td>
                     <td>মুদ্রা ও নোটের বিবরণ/ড্রাফট, পে-অর্ডার ও চেকের বিবরণ</td>
-                    <td class="vertical_align_middle">টাকা</td>
+                    <td class="vertical_align_middle" style="width:125px;">টাকা</td>
                     <td class="vertical_align_middle">পয়সা</td>
                 </tr>
                 <tr>
-                    <td>
+                    <td></td>
+                    <td></td>
+                    <td colspan="2" class="trade_vat">TRADE VAT FOR BAJAJ MOTORCYCLE</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td class="f-bold border_top_style">
                         <p>Monir</p>
                         <p>01644-928171</p>
                     </td>
-                    <td>
-                        <p>BAJAJ POINT</p>
+                    <td class="border_top_style">
+                        <h3>BAJAJ POINT</h3>
                         <p>400/B-A</p>
                         <p>MALIBAGH</p>
                         <p>CHOWDHURY</p>
                         <p>PARA, MALIBAGH,</p>
                         <p>DHAKA. BIN NO.</p>
-                        <p>0018831110202</p>
+                        <h3>0018831110202</h3>
                     </td>
                     <td>
                         <div class="left_chassis">
@@ -160,26 +195,31 @@
                             </div>
                             <div>
                                 <p>BAJAJ AVENGER 160 ABS</p>
-                            </div>
-                            <div>
                                 <p><span class="sl">1</span><span class="chassis">84540</span><span class="engine">39971</span></p>
                                 <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
-                                <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">3</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">4</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">5</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">6</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">7</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">8</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">9</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">10</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">11</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p>BAJAJ AVENGER 160 ABS</p>
+                                <p><span class="sl">13</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">14</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">15</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">16</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">17</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">18</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">19</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">20</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">21</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">22</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">23</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">24</span><span class="chassis">84540</span><span class="engine">39971</span></p>
+                                <p><span class="sl">25</span><span class="chassis">84540</span><span class="engine">39971</span></p>
                             </div>
                         </div>
                     </td>
@@ -190,17 +230,21 @@
                             </div>
                             <div>
                                 <p>BAJAJ AVENGER 160 ABS</p>
-                            </div>
-                            <div>
                                 <p><span class="sl">1</span><span class="chassis">84540</span><span class="engine">39971</span></p>
                                 <p><span class="sl">2</span><span class="chassis">84540</span><span class="engine">39971</span></p>
                             </div>
                         </div>
                     </td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td class="border_top_style">CASH</td>
+                    <td class="border_top_style">
+                        <p>1350 x 9</p>
+                        <p>1200 x 1</p>
+                        <p>1050 x 6</p>
+                        <p>975 x 3</p>
+                        <p>= 22,575</p>
+                    </td>
+                    <td class="border_top_style"></td>
+                    <td class="border_top_style"></td>
                 </tr>
                 <tr>
                     <td colspan="5" class="left_aligh">টাকা (কথায়)</td>
@@ -210,15 +254,18 @@
                 </tr>
                 <tr>
                     <td colspan="5" class="left_aligh">টাকা পাওয়া গেল</td>
-                    <td colspan="3" class="vertical_align_bottom">ম্যানেজার</td>
+                    <td colspan="3" class="vertical_align_bottom"></td>
                 </tr>
                 <tr>
-                    <td colspan="5" class="left_aligh" class="">তারিখ</td>
-                    <td colspan="3" class="vertical_align_bottom">বাংলাদেশ ব্যাংক/সোনালী ব্যাংক লিমিটেড</td>
+                    <td colspan="5" class="left_aligh" style="vertical-align: middle;">তারিখ</td>
+                    <td colspan="3" class="" style="border-top-style: hidden;">
+                        <p>ম্যানেজার</p>
+                        <p>বাংলাদেশ ব্যাংক/সোনালী ব্যাংক লিমিটেড</p>
+                    </td>
                 </tr>
             </tbody>
         </table>
-        <div class="footer_content">
+        <div class="footer_content" style="padding-left:10px;">
             <p>নোট: <span>1। সংশ্লিষ্ট দপ্তরের সহিত যোগাযোগ করিয়া সঠিক কোড নম্বর জানিয়া লইবেন।</span></p>
             <p><span>2। * যে সকল ক্ষেত্রে কর্মকর্তা কর্তৃক পৃষ্ঠাংকন প্রয়োজন সে সকল ক্ষেত্রে প্রযোজ্য হইবে।</span></p>
         </div>
