a5738dd91492a65fafa6c8148c12bbee2d060fc7
Md Monirul Islam <mmimonir.ctg@gmail.com>
2023-03-15 17:40:31 +0600

    skeleton load first bug fixes with display none

--- resources/views/service_dashboard_modals/add_parts.blade.php
+++ resources/views/service_dashboard_modals/add_parts.blade.php
@@ -1,5 +1,5 @@
 <!-- Modal Add-->
-<div class="modal fade" id="addParts" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade" id="addParts" tabindex="-1" role="dialog" aria-hidden="true" style="display:none;">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header text-write">
