d1c99ed1c566a1687ee510e14443d54add1d1cb2
Md Monirul Islam <mmimonir.ctg@gmail.com>
2023-03-17 17:18:46 +0600

    backpack update

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -31,7 +31,7 @@ return [
     // The string below will be passed through the url() helper.
     // - default: '' (project root)
     // - alternative: 'admin' (the admin's dashboard)
-    'home_link' => '',
+    'home_link' => 'admin',
 
     // Content of the HTML meta robots tag to prevent indexing and link following
     'meta_robots_content' => 'noindex, nofollow',
@@ -85,7 +85,7 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => '<b>Back</b>pack',
+    'project_logo'   => '<b>Bajaj Point</b>',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
