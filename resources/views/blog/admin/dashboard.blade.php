bd4da9a93f8261a12c06effe2ee76b7d71a519c5
Md Monirul Islam <mmimonir.ctg@gmail.com>
2023-03-25 21:11:51 +0600

    adminlte class updated

--- resources/views/blog/admin/dashboard.blade.php
+++ resources/views/blog/admin/dashboard.blade.php
@@ -2,9 +2,37 @@
 @section('title', 'Bajaj Point - 3S Dealer Of UttaraMotors Ltd')
 @push('page_css')
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-
 <!-- select2-bootstrap4-theme -->
 <link href="https://raw.githack.com/ttskch/select2-bootstrap4-theme/master/dist/select2-bootstrap4.css" rel="stylesheet">
+<style>
+    .select2-container--default .select2-selection--multiple .select2-selection__choice {
+        background-color: #007bff;
+        border-color: #006fe6;
+        color: #fff;
+        padding: 0 10px;
+        margin-top: .31rem;
+
+    }
+
+    .select2-container--default .select2-selection--multiple .select2-selection__choice__remove {
+        color: rgba(255, 255, 255, .7);
+        cursor: pointer;
+        display: inline-block;
+        font-weight: bold;
+        margin-right: 2px;
+
+    }
+
+    .select2-container--default .select2-results__option--highlighted[aria-selected] {
+        background-color: #007bff;
+        color: #fff;
+    }
+
+    .select2-container--default.select2-container--focus .select2-selection--multiple,
+    .select2-container--default.select2-container--focus .select2-selection--single {
+        border-color: #80bdff;
+    }
+</style>
 @endpush
 @section('content')
 <div class="container-fluid" style="margin-top:15px; padding:0;">
@@ -23,6 +51,16 @@
                     </select>
                 </div>
             </div>
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -37,7 +75,7 @@
     $(document).ready(function() {
         let tags = [];
         $(".select2").select2({
-            theme: 'classic',
+            theme: 'default',
             // width: 'style',
         });
     });
