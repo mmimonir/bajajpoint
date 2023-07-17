c8feea0f3bdb143913263ec754416810437347b3
Md Monirul Islam <mmimonir.ctg@gmail.com>
2023-07-17 14:57:39 +0600

    user role table added

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+    protected  $fillable = ['roles_name'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
